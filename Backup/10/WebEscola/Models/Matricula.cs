6065799630fb25170507b558584b86759284493b
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2020-05-25 21:32:35 -0300

    Trabalhando com Relacionamento - Update de Dados Relacionados

---
+++ Backup/10/WebEscola/Models/Matricula.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebEscola.Models
+{
+    public class Matricula
+    {
+        public int MatriculaID { get; set; }
+        public int AlunoID { get; set; }
+        public int CursoID { get; set; }
+        public Aluno Aluno { get; set; }
+        public Curso Curso { get; set; }
+    }
+}
